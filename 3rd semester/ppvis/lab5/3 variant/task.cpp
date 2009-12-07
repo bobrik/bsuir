f6b1f74cf78e247c930abbd1f227628eadd0d9dc
bobrik <ibobrik@gmail.com>
2009-12-08 00:40:42 +0200

    made Graph more abstract, moved tasks to GraphTasks; as you wish, mr. Uvarov! ;)

--- 3rd semester/ppvis/lab5/3 variant/task.cpp
+++ 3rd semester/ppvis/lab5/3 variant/task.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <set>
+#include <fstream>
 
 using namespace std;
 
@@ -10,127 +12,176 @@ enum incidency_type {EMPTY, DIRECT, INVERTED};
 
 class Graph
 {
-	private:
-		vector< vector<short int> * > * incidency;
-		bool _hasPath(short int, short int);
-		map<short int, bool> * _getAcessible(short int, short int = -1);
-	public:
-		Graph(vector< vector<short int> * > *);
-		bool isBlah();
+    private:
+        vector< vector<short int> * > * incidency;
+        bool _hasPath(short int, short int);
+        map<short int, bool> * _getAcessible(short int, short int = -1);
+    public:
+        Graph(string);
+        bool atLeastOneWayConnected();
+        int getVerticesCount();
+        set<int> * getIncident(int);
 };
 
-Graph::Graph(vector< vector<short int> * > * _incidency)
+class GraphTasks
 {
-	// external - edges
-	// internal - vertices
-	incidency = _incidency;
+    public:
+        static bool hasPath(Graph *, int, int);
+        static map<int, bool> * getAcessible(Graph *, int, int = -1);
+        static bool isAtLeastOneWayConnected(Graph *);
+};
+
+Graph::Graph(string filename)
+{
+    // external - edges
+    // internal - vertices
+    int vertices, edges;
+    ifstream in(filename.c_str());
+    in >> vertices >> edges;
+    // filling with «zeros»
+    incidency = new vector< vector <short int> * >;
+    for (int i = 0; i < edges; ++i)
+    {
+        vector<short int> * edge = new vector<short int>;
+        for (int j = 0; j < vertices; ++j)
+            edge->push_back(EMPTY);
+        incidency->push_back(edge);
+    }
+    // reading edges
+    int from, to;
+    vector< vector<short int> * >::iterator i;
+    for (i = incidency->begin(); i < incidency->end(); ++i)
+    {
+        in >> from >> to;
+        (*i)->at(from) = INVERTED;
+        (*i)->at(to)   = DIRECT;
+    }
 }
 
-bool Graph::isBlah()
+int Graph::getVerticesCount()
 {
-	map<short int, bool> * marked = _getAcessible(0);
-	bool answer = true;
-	int verticesCount = (*(incidency->begin()))->size();
-	for (int distance = 0; distance < verticesCount; ++distance)
-	{
-		if ((*marked)[distance] == false && !this->_hasPath(distance, 0))
-		{
-			answer = false;
-			break;
-		}
-	}
-	
-	delete marked;
-	
-	return answer;
+    return (*(incidency->begin()))->size();
 }
 
-map<short int, bool> * Graph::_getAcessible(short int start, short int end)
+set<int> * Graph::getIncident(int vertex)
 {
-	map<short int, bool> * marked = new map<short int, bool>;
-	queue<short int> * vertices = new queue<short int>;
-	vector< vector<short int> * >::iterator i;
-	vector<short int>::iterator j;
-	
-	int distance;
-	int verticesCount = (*(incidency->begin()))->size();
-	for (distance = 0; distance < verticesCount; ++ distance)
-	{
-		(*marked)[distance] = false;
-	}
-	(*marked)[start] = true;
-	
-	if (verticesCount > start)
-	{
-		vertices->push(start);
-	}
-	
-	short int vertex;
-	while (!vertices->empty())
-	{
-		vertex = vertices->front();
-		vertices->pop();
-		for (i = incidency->begin(); i < incidency->end(); ++i)
-		{
-			if ((*i)->at(vertex) == INVERTED)
-			{
-				// vertex hast edge starting from it and not yet marked
-				distance = 0;
-				for (j = (*i)->begin(); j < (*i)->end(); ++j, ++distance)
-				{
-					if (*j == DIRECT && vertex != distance)
-					{
-						vertices->push(distance);
-						if (end == distance)
-						{
-							(*marked)[end] = true;
-							while (!vertices->empty())
-							{
-								vertices->pop();
-							}
-							break;
-						}
-					}
-				}
-			}
-		}
-		(*marked)[vertex] = true;
-	}
-	
-	delete vertices;
-	
-	return marked;
+    vector< vector<short int> * >::iterator i;
+    vector<short int>::iterator j;
+    set<int> * incident = new set<int>;
+    for (i = incidency->begin(); i < incidency->end(); ++i)
+    {
+        if ((*i)->at(vertex) == INVERTED)
+        {
+            int vertexTo = 0;
+            for (j = (*i)->begin(); j < (*i)->end(); ++j, ++vertexTo)
+            {
+                if (*j == DIRECT)
+                {
+                    incident->insert(vertexTo);
+                }
+            }
+        }
+    }
+    return incident;
 }
 
-bool Graph::_hasPath(short int first, short int second)
+bool GraphTasks::isAtLeastOneWayConnected(Graph * graph)
 {
-	map<short int, bool> * marked = _getAcessible(first, second);
-	
-	bool answer = false;
-	if ((*marked)[second])
-	{
-		answer = true;
-	}
-	
-	delete marked;
-	return answer;
+    bool answer = true;
+    int verticesCount = graph->getVerticesCount();
+    map<int, bool> * marked;
+    for (int current = 0; current < verticesCount; ++current)
+    {
+        marked = GraphTasks::getAcessible(graph, current);
+        for (int distance = 0; distance < verticesCount; ++distance)
+        {
+            if (distance == current)
+                continue;
+            if ((*marked)[distance] == false && !GraphTasks::hasPath(graph, distance, current))
+            {
+                answer = false;
+                break;
+            }
+        }
+
+        delete marked;
+
+        if (!answer)
+            break;
+    }
+
+    return answer;
+}
+
+map<int, bool> * GraphTasks::getAcessible(Graph * graph, int start, int end)
+{
+    map<int, bool> * marked = new map<int, bool>;
+    queue<int> * vertices = new queue<int>;
+    set<int> * incident;
+    set<int>::iterator i;
+    
+    int distance;
+    int verticesCount = graph->getVerticesCount();
+    for (distance = 0; distance < verticesCount; ++ distance)
+    {
+        (*marked)[distance] = false;
+    }
+    (*marked)[start] = true;
+    
+    vertices->push(start);
+    
+    short int vertex;
+    while (!vertices->empty())
+    {
+        vertex = vertices->front();
+        vertices->pop();
+        incident = graph->getIncident(vertex);
+        for (i = incident->begin(); i != incident->end(); ++i)
+        {
+            if (!(*marked)[(*i)])
+            {
+                if (end == (*i))
+                {
+                    (*marked)[end] = true;
+                    while (!vertices->empty())
+                    {
+                        vertices->pop();
+                    }
+                    break;
+                } else
+                {
+                    vertices->push(*i);
+                }
+            }
+        }
+        delete incident;
+        (*marked)[vertex] = true;
+    }
+    
+    delete vertices;
+    
+    return marked;
+}
+
+bool GraphTasks::hasPath(Graph * graph, int start, int end)
+{
+    map<int, bool> * marked = GraphTasks::getAcessible(graph, start, end);
+    
+    bool answer = false;
+    if ((*marked)[end])
+    {
+        answer = true;
+    }
+    
+    delete marked;
+    return answer;
 }
 
 
 int main()
 {
-	vector< vector<short int> * > * incidency = new vector< vector<short int> * >;
-	vector<short int> * edge_0 = new vector<short int>;
-	edge_0->push_back(INVERTED);
-	edge_0->push_back(DIRECT);
-	edge_0->push_back(EMPTY);
-	vector<short int> * edge_1 = new vector<short int>;
-	edge_1->push_back(EMPTY);
-	edge_1->push_back(INVERTED);
-	edge_1->push_back(DIRECT);
-	incidency->push_back(edge_0);
-	incidency->push_back(edge_1);
-	Graph graf(incidency);
-	cout << "ololo: " << graf.isBlah() << endl;
-	return 0;
+    Graph graph("task.in");
+    
+    cout << "isAtLeastOneWayConnected: " << GraphTasks::isAtLeastOneWayConnected(&graph) << endl;
+    return 0;
 }
