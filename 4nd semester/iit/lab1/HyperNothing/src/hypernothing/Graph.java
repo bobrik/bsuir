3acf477bc735226b31fc884547e18db2350e1326
bobrik <ibobrik@gmail.com>
2010-02-22 23:21:39 +0200

    first lab for iit in 4nd semester. useless, but in java. bad code, but works :)

---
+++ 4nd semester/iit/lab1/HyperNothing/src/hypernothing/Graph.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hypernothing;
+
+import java.util.Stack;
+import java.util.Vector;
+import java.util.HashMap;
+
+/**
+ *
+ * @author bobrik
+ */
+public class Graph
+{
+    private Vector  nodes;
+    private HashMap nodeConnections;
+
+    public Graph(Vector elements, HashMap elementConnections)
+    {
+	this.nodes = elements;
+	this.nodeConnections = elementConnections;
+    }
+
+    public int maxDistance(Object start, Object end)
+    {
+	// NP-complete task
+	if (!nodes.contains(start) || !nodes.contains(end))
+	{
+	    return 0;
+	}
+
+	Vector nodeWeights = new Vector();
+	for (int i = 0; i < nodes.size(); ++i)
+	{
+	    nodeWeights.addElement(0);
+	}
+
+	Stack currentPath = new Stack();
+
+	currentPath.addElement(start);
+
+	_maxDistanceHelper(currentPath, nodeWeights, end);
+
+	return new Integer(nodeWeights.elementAt(nodes.indexOf(end)).toString());
+    }
+
+    private void _maxDistanceHelper(Stack currentPath, Vector nodeWeights, Object end)
+    {
+	Object currentNode = currentPath.peek();
+	for (int i = 0; i < ((Vector) nodeConnections.get(currentNode)).size(); ++i)
+	{
+	    Object nodeConnection = ((Vector) nodeConnections.get(currentNode)).elementAt(i);
+	    if (!currentPath.contains(nodeConnection))
+	    {
+		if (new Integer(nodeWeights.elementAt(nodes.indexOf(nodeConnection)).toString()) < currentPath.size())
+		{
+		    nodeWeights.setElementAt(currentPath.size(), nodes.indexOf(nodeConnection));
+
+		    if (!nodeConnection.equals(end))
+		    {
+			currentPath.push(nodeConnection);
+			_maxDistanceHelper(currentPath, nodeWeights, end);
+		    }
+		}
+	    }
+	}
+	nodeWeights.setElementAt(0, nodes.indexOf(currentNode)); // let's give that node another chanse
+	currentPath.pop();
+    }
+}
