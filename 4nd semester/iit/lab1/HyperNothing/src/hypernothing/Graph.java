107eba09bd58f764c3811e0b8bd7649d8a49de6e
bobrik <ibobrik@gmail.com>
2010-05-30 17:11:26 +0300

    changed something. i don't remember :)

--- 4nd semester/iit/lab1/HyperNothing/src/hypernothing/Graph.java
+++ 4nd semester/iit/lab1/HyperNothing/src/hypernothing/Graph.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 
+
 package hypernothing;
 
 import java.util.Stack;
@@ -10,26 +11,43 @@ import java.util.Vector;
 import java.util.HashMap;
 
 /**
- *
+ * Graph representation class
  * @author bobrik
  */
 public class Graph
 {
-    private Vector  nodes;
-    private HashMap nodeConnections;
+    /**
+     * Nodes list
+     */
+    private Vector<String>  nodes;
+    /**
+     * Connections map
+     */
+    private HashMap< String, Vector<String> > nodeConnections;
 
+    /**
+     * Create constant Graph object with prefilled elements and connections
+     * @param elements vector of elements
+     * @param elementConnections hash of connections
+     */
     public Graph(Vector elements, HashMap elementConnections)
     {
 	this.nodes = elements;
 	this.nodeConnections = elementConnections;
     }
 
-    public int maxDistance(Object start, Object end)
+    /**
+     * Calculate max distance bewteen vertices
+     * @param start first vertex name
+     * @param end last vertex name
+     * @return path length
+     */
+    public String maxDistance(String start, String end)
     {
 	// NP-complete task
 	if (!nodes.contains(start) || !nodes.contains(end))
 	{
-	    return 0;
+	    return "No path";
 	}
 
 	Vector<Integer> nodeWeights = new Vector<Integer>();
@@ -38,21 +56,71 @@ public class Graph
 	    nodeWeights.addElement(0);
 	}
 
-	Stack currentPath = new Stack();
+	Stack<String> currentPath = new Stack<String>();
 
 	currentPath.addElement(start);
 
 	_maxDistanceHelper(currentPath, nodeWeights, end);
 
-	return new Integer(nodeWeights.elementAt(nodes.indexOf(end)).toString());
+	int maxNodeWeight = new Integer(nodeWeights.elementAt(nodes.indexOf(end)).toString());
+
+	Vector<String> path = new Vector<String>();
+
+	path.add(end);
+
+	while (true)
+	{
+	    try
+	    {
+		path.add(nodes.elementAt(_prevStepNodeNumber(path.lastElement() , nodeWeights)));
+	    } catch (Exception e)
+	    {
+		break;
+	    }
+	}
+
+	String result = new String("\n");
+
+	for (int i = path.size()-1; i > 0; --i)
+	{
+	    result += path.elementAt(i) + " -(1)> \n";
+	}
+
+	if (path.size() > 0)
+	{
+	    result += path.elementAt(0);
+	}
+
+	return result;
     }
 
-    private void _maxDistanceHelper(Stack currentPath, Vector<Integer> nodeWeights, Object end)
+    private int _prevStepNodeNumber(String node, Vector<Integer> nodeWeights) throws Exception
     {
-	Object currentNode = currentPath.peek();
-	for (int i = 0; i < ((Vector) nodeConnections.get(currentNode)).size(); ++i)
+	for (int i = 0; i < nodes.size(); ++i)
 	{
-	    Object nodeConnection = ((Vector) nodeConnections.get(currentNode)).elementAt(i);
+	    Vector<String> connections = (Vector) nodeConnections.get(nodes.elementAt(i));
+	    for (int j = 0; j < connections.size(); ++j)
+	    {
+		String currentNode = connections.elementAt(j);
+		if (!currentNode.equals(node))
+		{
+		    continue;
+		}
+		if (nodeWeights.elementAt(i) == nodeWeights.elementAt(nodes.indexOf(node))-1)
+		{
+		    return i;
+		}
+	    }
+	}
+	throw new Exception("No way back!");
+    }
+
+    private void _maxDistanceHelper(Stack<String> currentPath, Vector<Integer> nodeWeights, String end)
+    {
+	String currentNode = currentPath.peek();
+	for (int i = 0; i < nodeConnections.get(currentNode).size(); ++i)
+	{
+	    String nodeConnection = nodeConnections.get(currentNode).elementAt(i);
 	    if (!currentPath.contains(nodeConnection))
 	    {
 		if (nodeWeights.elementAt(nodes.indexOf(nodeConnection)) < currentPath.size())
@@ -67,7 +135,7 @@ public class Graph
 		}
 	    }
 	}
-	nodeWeights.setElementAt(0, nodes.indexOf(currentNode)); // let's give that node another chanse
+	//nodeWeights.setElementAt(0, nodes.indexOf(currentNode)); // let's give that node another chanse
 	currentPath.pop();
     }
 }
